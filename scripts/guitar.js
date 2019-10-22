78f7f644741ce9ab6701fff59a952e667d3f80a0
Catherine Zeng <yczeng@mit.edu>
2019-10-22 14:22:46 -0400

    got rid of ugly ass innerHTML approach

--- scripts/guitar.js
+++ scripts/guitar.js
@@ -1,7 +1,8 @@
-var garbageDivs = ["mS99P", "_21_lj", "ztjWI _2GydY", "eDpei", "js-ab-regular", "ztjWI _2GydY", "_1Dc-f", "comments", "_3aR1y_3D2BN", "_2e92L_17oyS", "o7EYP", "_2MgSS _1ScGs"]
+var garbageDivs = ["mS99P", "_21_lj", "ztjWI _2GydY", "eDpei", "js-ab-regular", "ztjWI _2GydY", "_1Dc-f", "comments", "_3aR1y_3D2BN", "_2e92L_17oyS", "o7EYP", "_2MgSS _1ScGs", "_3aR1y _3D2BN", "_1wRYH"]
 
 for (i = 0; i<garbageDivs.length; i++){
-	if (document.getElementsByClassName(garbageDivs[i])){
-		document.getElementsByClassName(garbageDivs[i])[0].innerHTML = ""
+	let element = document.getElementsByClassName(garbageDivs[i])
+	if (element[0]){
+		element[0].parentNode.removeChild(element[0]);
 	}
 }
