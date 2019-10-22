e64a2f2d399cd273a3af0c81473e8ef984499a5e
Catherine Zeng <yczeng@mit.edu>
2019-10-22 14:28:18 -0400

    got rid of the autoscroll/ popup shit through bruteforce approach

--- scripts/guitar.js
+++ scripts/guitar.js
@@ -1,8 +1,9 @@
-var garbageDivs = ["mS99P", "_21_lj", "ztjWI _2GydY", "eDpei", "js-ab-regular", "ztjWI _2GydY", "_1Dc-f", "comments", "_3aR1y_3D2BN", "_2e92L_17oyS", "o7EYP", "_2MgSS _1ScGs", "_3aR1y _3D2BN", "_1wRYH"]
-
-for (i = 0; i<garbageDivs.length; i++){
-	let element = document.getElementsByClassName(garbageDivs[i])
-	if (element[0]){
-		element[0].parentNode.removeChild(element[0]);
+var loop = setInterval(function() {
+	var garbageDivs = ["mS99P", "_21_lj", "ztjWI _2GydY", "eDpei", "js-ab-regular", "ztjWI _2GydY", "_1Dc-f", "comments", "_3aR1y_3D2BN", "_2e92L_17oyS", "o7EYP", "_2MgSS _1ScGs", "_3aR1y _3D2BN", "_1wRYH", "_38pBs", "_2e92L _17oyS"]
+	for (i = 0; i<garbageDivs.length; i++){
+		let element = document.getElementsByClassName(garbageDivs[i])
+		if (element[0]){
+			element[0].parentNode.removeChild(element[0]);
+		}
 	}
-}
+}, 100);
