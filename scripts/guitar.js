324e85f3190a4f0b1e03877139f6212c45b74fdd
Catherine Zeng <yczeng@mit.edu>
2019-10-23 22:21:53 -0400

    site updated, extension broke, now it works again

--- scripts/guitar.js
+++ scripts/guitar.js
@@ -1,9 +1,15 @@
+var garbageDivs = ["mS99P", "_21_lj", "ztjWI _2GydY", "eDpei", "js-ab-regular", "ztjWI _2GydY", "_1Dc-f", "comments", "_3aR1y_3D2BN", "_2e92L_17oyS", "o7EYP", "_2MgSS _1ScGs", "_3aR1y _3D2BN", "_1wRYH", "_38pBs", "_2e92L _17oyS", ""]
+for (i = 0; i<garbageDivs.length; i++){
+	let element = document.getElementsByClassName(garbageDivs[i])
+	if (element[0]){
+		element[0].parentNode.removeChild(element[0]);
+	}
+}
+
+// this is literally just a hack to get rid of that annoying autoscroll javascript thing
 var loop = setInterval(function() {
-	var garbageDivs = ["mS99P", "_21_lj", "ztjWI _2GydY", "eDpei", "js-ab-regular", "ztjWI _2GydY", "_1Dc-f", "comments", "_3aR1y_3D2BN", "_2e92L_17oyS", "o7EYP", "_2MgSS _1ScGs", "_3aR1y _3D2BN", "_1wRYH", "_38pBs", "_2e92L _17oyS"]
-	for (i = 0; i<garbageDivs.length; i++){
-		let element = document.getElementsByClassName(garbageDivs[i])
-		if (element[0]){
-			element[0].parentNode.removeChild(element[0]);
-		}
+	let element = document.getElementsByClassName("_2MgSS _1ScGs")
+	if (element[0]){
+		element[0].parentNode.removeChild(element[0]);
 	}
 }, 100);
